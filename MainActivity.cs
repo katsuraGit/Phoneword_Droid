0dadfe9ecae9719f9c93753607bd36e02bc18769
katsuraGit <kaito@microsoft.com>
2017-01-06 10:50:29 +0900

    New Xamrin code

---
+++ MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Phoneword_Droid
+{
+    [Activity(Label = "Phoneword_Droid", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
